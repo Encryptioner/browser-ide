be8181f5400e33c2632d145c2def269d4876b88a
Ankur Mursalin <mir.ankur,ruet13@gmail.com>
2026-02-23 11:13:33 +0600

    feat: update ci

--- playwright.config.ts
+++ playwright.config.ts
@@ -15,6 +15,7 @@ import { defineConfig, devices } from '@playwright/test';
  */
 export default defineConfig({
   testDir: './tests/e2e',
+  testIgnore: ['**/visual-regression*'],
   fullyParallel: true,
   forbidOnly: !!process.env.CI,
   retries: process.env.CI ? 2 : 0,
